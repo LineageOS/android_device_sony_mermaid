c81dcfd827a28ed55582c5b9a457bfd099d5329e
LuK1337 <priv.luk@gmail.com>
2019-05-09 01:53:00 +0200

    mermaid: Enable aux camera for Snap
    Change-Id: I385bc59359f21b269390e3941f32436b362627b2

--- device.mk
+++ device.mk
@@ -25,7 +25,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
