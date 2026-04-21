4ff50dceef76142eee5a58df22836db3271253ce
LuK1337 <priv.luk@gmail.com>
2026-04-21 14:51:54 +0200

    mermaid: Use libcrypto-v33 for secd
    F DEBUG   : Kernel Release: '4.19.325-cip130-st14-perf-g9de8f95217f2'
    F DEBUG   : Revision: '0'
    F DEBUG   : ABI: 'arm64'
    F DEBUG   : Timestamp: 2026-04-21 14:45:43.412405660+0200
    F DEBUG   : Process uptime: 1s
    F DEBUG   : Executable: /vendor/bin/hw/vendor.semc.hardware.secd@1.0-service
    F DEBUG   : Cmdline: /vendor/bin/hw/vendor.semc.hardware.secd@1.0-service
    F DEBUG   : pid: 3755, tid: 3756, name: vendor.semc.har  >>> /vendor/bin/hw/vendor.semc.hardware.secd@1.0-service <<<
    F DEBUG   : uid: 1000
    F DEBUG   : signal 6 (SIGABRT), code -1 (SI_QUEUE), fault addr --------
    F DEBUG   : Abort message: 'stack corruption detected (-fstack-protector)'
    F DEBUG   :     x0  0000000000000000  x1  0000000000000eac  x2  0000000000000006  x3  0000007adad01300
    F DEBUG   :     x4  607372652c271f63  x5  607372652c271f63  x6  607372652c271f63  x7  7f7f7f7f7f7f7f7f
    F DEBUG   :     x8  00000000000000f0  x9  8db2ff94a6444142  x10 000000ff00000020  x11 000000003b9ac9ff
    F DEBUG   :     x12 000000004a011c4b  x13 0000000034155555  x14 0000000000000018  x15 0000000049ff3b28
    F DEBUG   :     x16 0000007d72fc1120  x17 0000007d72faa040  x18 0000007ada38c000  x19 0000000000000eab
    F DEBUG   :     x20 0000000000000eac  x21 00000000ffffffff  x22 000000000000000e  x23 0000000000000001
    F DEBUG   :     x24 000000000000000e  x25 0000007adad01f80  x26 0000007adad01fc0  x27 0000007adac09000
    F DEBUG   :     x28 0000007adac08000  x29 0000007adad01380
    F DEBUG   :     lr  0000007d72f4b48c  sp  0000007adad01300  pc  0000007d72f4b4b0  pst 0000000000000000
    F DEBUG   :     esr 0000000000000000
    F DEBUG   : 3 total frames
    F DEBUG   : backtrace:
    F DEBUG   :       #00 pc 00000000000724b0  /apex/com.android.runtime/lib64/bionic/libc.so (abort+156) (BuildId: d344d60b33aa499337d3af6b6a096b00)
    F DEBUG   :       #01 pc 0000000000098a34  /apex/com.android.runtime/lib64/bionic/libc.so (__stack_chk_fail+20) (BuildId: d344d60b33aa499337d3af6b6a096b00)
    F DEBUG   :       #02 pc 000000000000d578  /vendor/bin/hw/vendor.semc.hardware.secd@1.0-service (CredMgr_Crypto_Utils_HashHmacSha1+780) (BuildId: 45a6c57b4738488b26380af5ee7166da)
    
    Change-Id: I8b4ac9f536c388a00dec46e0e3d47127eeb7b271

--- extract-files.py
+++ extract-files.py
@@ -21,6 +21,8 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/hw/vendor.semc.hardware.secd@1.0-service': blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
